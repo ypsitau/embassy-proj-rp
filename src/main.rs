9feeae530f59a92770c3e389a7101845b4572546
ypsitau <ypsitau@nifty.com>
2026-09-04 15:31:15 +0900

    updated

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,66 @@
 use defmt::info;
 use embassy_executor::Spawner;
 use embassy_rp::gpio;
+{% if use_usb_driver %}
+use embassy_usb as usb;
+{% endif %}
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
+    {% if use_usb_driver %}
+    let mut usb_builder = {
+        const VID: u16 = 0xc0de;
+        const PID: u16 = 0xcafe;
+        const CONFIG_DESCRIPTOR_SIZE: usize = 256;
+        const BOS_DESCRIPTOR_SIZE: usize = 256;
+        const MSOS_DESCRIPTOR_SIZE: usize = 256;
+        const CONTROL_BUF_SIZE: usize = 64;
+        let mut config = usb::Config::new(VID, PID);
+        config.manufacturer = Some("Embassy");
+        config.product = Some("{{project-name}}");
+        config.serial_number = Some("12345678");
+        config.max_power = 100;
+        config.max_packet_size_0 = CONTROL_BUF_SIZE as u8;
+        let config_descriptor_buf = { // should be replaced by make_static macro when it becomes available
+            static STATIC_CELL: StaticCell<[u8; CONFIG_DESCRIPTOR_SIZE]> = StaticCell::new();
+            STATIC_CELL.init([0; CONFIG_DESCRIPTOR_SIZE])
+        };
+        let bos_descriptor_buf = { // should be replaced by make_static macro when it becomes available
+            static STATIC_CELL: StaticCell<[u8; BOS_DESCRIPTOR_SIZE]> = StaticCell::new();
+            STATIC_CELL.init([0; BOS_DESCRIPTOR_SIZE])
+        };
+        let msos_descriptor_buf = { // should be replaced by make_static macro when it becomes available
+            static STATIC_CELL: StaticCell<[u8; MSOS_DESCRIPTOR_SIZE]> = StaticCell::new();
+            STATIC_CELL.init([0; MSOS_DESCRIPTOR_SIZE])
+        };
+        let control_buf = { // should be replaced by make_static macro when it becomes available
+            static STATIC_CELL: StaticCell<[u8; CONTROL_BUF_SIZE]> = StaticCell::new();
+            STATIC_CELL.init([0; CONTROL_BUF_SIZE])
+        };
+        let device_handler = { // should be replaced by make_static macro when it becomes available
+            static STATIC_CELL: StaticCell<DeviceHandler> = StaticCell::new();
+            STATIC_CELL.init(DeviceHandler::new())
+        };
+        let mut usb_builder = usb::Builder::new(usb_driver, config,
+            config_descriptor_buf, bos_descriptor_buf, msos_descriptor_buf, control_buf);
+        //usb_builder.handler(device_handler);
+        usb_builder
+    };
+    let cdc_driver = {
+        let state = {
+            static STATE: StaticCell<usb::class::cdc_acm::State> = StaticCell::new();
+            STATE.init(usb::class::cdc_acm::State::new())
+        };
+        let max_packet_size = 64;
+        usb::class::cdc_acm::CdcAcmClass::new(&mut usb_builder, state, max_packet_size)
+    };
+    let mut usb_device = usb_builder.build();
+    let fut_usb = usb_device.run();
+    let (cdc_sender, cdc_receiver) = cdc_driver.split();
+    {% endif %}
     let mut gpio_led = gpio::Output::new(p.PIN_25, gpio::Level::Low);
     loop {
         info!("led on!");
