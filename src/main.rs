45c22711c9b47becc227cd89b24f20d20c02ee2d
ypsitau <ypsitau@nifty.com>
2026-09-05 16:29:32 +0900

    implementing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+use core::sync::atomic;
 use defmt::info;
 use embassy_executor::Spawner;
 use embassy_rp as rp;
@@ -33,29 +34,29 @@ async fn main(_spawner: Spawner) {
         config.serial_number = Some("12345678");
         config.max_power = 100;
         config.max_packet_size_0 = CONTROL_BUF_SIZE as u8;
-        let config_descriptor_buf = { // should be replaced by make_static macro when it becomes available
+        let config_descriptor_buf = {
             static STATIC_CELL: StaticCell<[u8; CONFIG_DESCRIPTOR_SIZE]> = StaticCell::new();
-            STATIC_CELL.init([0; CONFIG_DESCRIPTOR_SIZE])
+            STATIC_CELL.init([0u8; CONFIG_DESCRIPTOR_SIZE])
         };
-        let bos_descriptor_buf = { // should be replaced by make_static macro when it becomes available
+        let bos_descriptor_buf = {
             static STATIC_CELL: StaticCell<[u8; BOS_DESCRIPTOR_SIZE]> = StaticCell::new();
-            STATIC_CELL.init([0; BOS_DESCRIPTOR_SIZE])
+            STATIC_CELL.init([0u8; BOS_DESCRIPTOR_SIZE])
         };
-        let msos_descriptor_buf = { // should be replaced by make_static macro when it becomes available
+        let msos_descriptor_buf = {
             static STATIC_CELL: StaticCell<[u8; MSOS_DESCRIPTOR_SIZE]> = StaticCell::new();
-            STATIC_CELL.init([0; MSOS_DESCRIPTOR_SIZE])
+            STATIC_CELL.init([0u8; MSOS_DESCRIPTOR_SIZE])
         };
-        let control_buf = { // should be replaced by make_static macro when it becomes available
+        let control_buf = {
             static STATIC_CELL: StaticCell<[u8; CONTROL_BUF_SIZE]> = StaticCell::new();
-            STATIC_CELL.init([0; CONTROL_BUF_SIZE])
+            STATIC_CELL.init([0u8; CONTROL_BUF_SIZE])
         };
-        //let device_handler = { // should be replaced by make_static macro when it becomes available
-        //    static STATIC_CELL: StaticCell<DeviceHandler> = StaticCell::new();
-        //    STATIC_CELL.init(DeviceHandler::new())
-        //};
-        let usb_builder = usb::Builder::new(usb_driver, config,
+        let usb_handler = {
+            static STATIC_CELL: StaticCell<USBHandler> = StaticCell::new();
+            STATIC_CELL.init(USBHandler::new())
+        };
+        let mut usb_builder = usb::Builder::new(usb_driver, config,
             config_descriptor_buf, bos_descriptor_buf, msos_descriptor_buf, control_buf);
-        //usb_builder.handler(device_handler);
+        usb_builder.handler(usb_handler);
         usb_builder
     };
     let cdc_driver = {
@@ -70,7 +71,6 @@ async fn main(_spawner: Spawner) {
     let fut_usb = usb_device.run();
     let fut_echo = async {
         let (mut cdc_sender, mut cdc_receiver) = cdc_driver.split();
-        let mut first = true;
         let buf = {
             static STATIC_CELL: StaticCell<[u8; 64]> = StaticCell::new();
             STATIC_CELL.init([0u8; 64])
@@ -82,17 +82,13 @@ async fn main(_spawner: Spawner) {
                 let buf_read = match cdc_receiver.read_packet(buf).await {
                     Ok(n) => &buf[..n], Err(e) => break e,
                 };
-                if first {
-                    if let Err(e) = cdc_sender.write_packet(b"\r\nEcho via USB\r\n").await { break e; }
-                    first = false;
-                }
                 if let Err(e) = cdc_sender.write_packet(buf_read).await { break e; }
             };
             if e != usb::driver::EndpointError::Disabled { break; }
         };
     };
     {% endif -%}
-    let fut_gpio = async {
+    let fut_blinky = async {
         let mut gpio_led = rp::gpio::Output::new(p.PIN_25, rp::gpio::Level::Low);
         loop {
             gpio_led.set_high();
@@ -103,8 +99,44 @@ async fn main(_spawner: Spawner) {
     };
     info!("Starting main loop");
     {% if use_usb_driver -%}
-    embassy_futures::join::join3(fut_usb, fut_echo, fut_gpio).await;
+    embassy_futures::join::join3(fut_usb, fut_echo, fut_blinky).await;
 {% else -%}
-    fut_gpio.await;
+    fut_blinky.await;
 {% endif -%}
+}
+
+//-----------------------------------------------------------------------------
+// USBHandler
+//-----------------------------------------------------------------------------
+struct USBHandler {
+    configured: atomic::AtomicBool,
+}
+
+impl USBHandler {
+    fn new() -> Self {
+        USBHandler { configured: atomic::AtomicBool::new(false), }
+    }
+}
+
+impl usb::Handler for USBHandler {
+    /// Called when the USB device has been enabled or disabled.
+    fn enabled(&mut self, enabled: bool) {
+        info!("usb::Handler.enabled({})", enabled);
+        self.configured.store(false, atomic::Ordering::Relaxed);
+    }
+    /// Called after a USB reset after the bus reset sequence is complete.
+    fn reset(&mut self) {
+        info!("usb::Handler.reset()");
+        self.configured.store(false, atomic::Ordering::Relaxed);
+    }
+    /// Called when the host has set the address of the device to `addr`.
+    fn addressed(&mut self, addr: u8) {
+        info!("usb::Handler.addressed(addr: {})", addr);
+        self.configured.store(false, atomic::Ordering::Relaxed);
+    }
+    /// Called when the host has enabled or disabled the configuration of the device.
+    fn configured(&mut self, configured: bool) {
+        info!("usb::Handler.configured(configured: {})", configured);
+        self.configured.store(configured, atomic::Ordering::Relaxed);
+    }
 }
