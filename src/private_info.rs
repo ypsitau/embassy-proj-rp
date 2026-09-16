1f05d4f5bbcd2064da50d82168c34f3787529c9c
ypsitau <ypsitau@nifty.com>
2026-09-16 16:01:01 +0900

    updated

--- src/private_info.rs
+++ src/private_info.rs
@@ -1,2 +1,2 @@
-pub const WIFI_NETWORK: &str = "SSID"; // change to your network SSID
+pub const WIFI_SSID: &str = "SSID"; // change to your network SSID
 pub const WIFI_PASSWORD: &str = "PASSWORD"; // change to your network password
