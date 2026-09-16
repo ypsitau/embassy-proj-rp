9472efba1315bf0c86ca5b666263d4c8d0896b45
ypsitau <ypsitau@nifty.com>
2026-09-16 15:31:21 +0900

    updated

---
+++ src/templates/usb_device_cdc.rs
@@ -0,0 +1,125 @@
+use core::sync::atomic;
+use defmt::info;
+use embassy_executor::Spawner;
+use embassy_rp as rp;
+use embassy_usb as usb;
+use static_cell::StaticCell;
+use {defmt_rtt as _, panic_probe as _};
+
+rp::bind_interrupts!(struct Irqs {
+    USBCTRL_IRQ => rp::usb::InterruptHandler<rp::peripherals::USB>;
+});
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let (usb_driver,) = {
+        let p = rp::init(Default::default());
+        let usb_driver = rp::usb::Driver::new(p.USB, Irqs);
+        (usb_driver,)
+    };
+    let fut_task_usb = task_usb(usb_driver);
+    fut_task_usb.await;
+}
+
+async fn task_usb(usb_driver: impl usb::driver::Driver<'static>) {
+    let mut usb_builder = {
+        const VID: u16 = 0xc0de;
+        const PID: u16 = 0xcafe;
+        const CONFIG_DESCRIPTOR_SIZE: usize = 256;
+        const BOS_DESCRIPTOR_SIZE: usize = 256;
+        const MSOS_DESCRIPTOR_SIZE: usize = 256;
+        const CONTROL_BUF_SIZE: usize = 64;
+        let mut usb_config = usb::Config::new(VID, PID);
+        usb_config.manufacturer = Some("Embassy");
+        usb_config.product = Some("hoge");
+        usb_config.serial_number = Some("12345678");
+        usb_config.max_power = 100;
+        usb_config.max_packet_size_0 = CONTROL_BUF_SIZE as u8;
+        let config_descriptor_buf = {
+            static STATIC_CELL: StaticCell<[u8; CONFIG_DESCRIPTOR_SIZE]> = StaticCell::new();
+            STATIC_CELL.init([0u8; CONFIG_DESCRIPTOR_SIZE])
+        };
+        let bos_descriptor_buf = {
+            static STATIC_CELL: StaticCell<[u8; BOS_DESCRIPTOR_SIZE]> = StaticCell::new();
+            STATIC_CELL.init([0u8; BOS_DESCRIPTOR_SIZE])
+        };
+        let msos_descriptor_buf = {
+            static STATIC_CELL: StaticCell<[u8; MSOS_DESCRIPTOR_SIZE]> = StaticCell::new();
+            STATIC_CELL.init([0u8; MSOS_DESCRIPTOR_SIZE])
+        };
+        let control_buf = {
+            static STATIC_CELL: StaticCell<[u8; CONTROL_BUF_SIZE]> = StaticCell::new();
+            STATIC_CELL.init([0u8; CONTROL_BUF_SIZE])
+        };
+        let usb_handler = {
+            static STATIC_CELL: StaticCell<USBHandler> = StaticCell::new();
+            STATIC_CELL.init(USBHandler::new())
+        };
+        let mut usb_builder = usb::Builder::new(usb_driver, usb_config,
+            config_descriptor_buf, bos_descriptor_buf, msos_descriptor_buf, control_buf);
+        usb_builder.handler(usb_handler);
+        usb_builder
+    };
+    let cdc_driver = {
+        let state = {
+            static STATE: StaticCell<usb::class::cdc_acm::State> = StaticCell::new();
+            STATE.init(usb::class::cdc_acm::State::new())
+        };
+        let max_packet_size = 64;
+        usb::class::cdc_acm::CdcAcmClass::new(&mut usb_builder, state, max_packet_size)
+    };
+    let mut usb_device = usb_builder.build();
+    let fut_usb = usb_device.run();
+    let fut_echo = async {
+        let (mut cdc_sender, mut cdc_receiver) = cdc_driver.split();
+        let buf = {
+            static STATIC_CELL: StaticCell<[u8; 64]> = StaticCell::new();
+            STATIC_CELL.init([0u8; 64])
+        };
+        loop {
+            cdc_receiver.wait_connection().await;
+            info!("Connected");
+            let e = loop {
+                let buf_read = match cdc_receiver.read_packet(buf).await {
+                    Ok(n) => &buf[..n], Err(e) => break e,
+                };
+                if let Err(e) = cdc_sender.write_packet(buf_read).await { break e; }
+            };
+            if e != usb::driver::EndpointError::Disabled { break; }
+        };
+    };
+    embassy_futures::join::join(fut_usb, fut_echo).await;
+}
+
+struct USBHandler {
+    configured: atomic::AtomicBool,
+}
+
+impl USBHandler {
+    fn new() -> Self {
+        USBHandler { configured: atomic::AtomicBool::new(false), }
+    }
+}
+
+impl usb::Handler for USBHandler {
+    /// Called when the USB device has been enabled or disabled.
+    fn enabled(&mut self, enabled: bool) {
+        info!("usb::Handler.enabled({})", enabled);
+        self.configured.store(false, atomic::Ordering::Relaxed);
+    }
+    /// Called after a USB reset after the bus reset sequence is complete.
+    fn reset(&mut self) {
+        info!("usb::Handler.reset()");
+        self.configured.store(false, atomic::Ordering::Relaxed);
+    }
+    /// Called when the host has set the address of the device to `addr`.
+    fn addressed(&mut self, addr: u8) {
+        info!("usb::Handler.addressed(addr: {})", addr);
+        self.configured.store(false, atomic::Ordering::Relaxed);
+    }
+    /// Called when the host has enabled or disabled the configuration of the device.
+    fn configured(&mut self, configured: bool) {
+        info!("usb::Handler.configured(configured: {})", configured);
+        self.configured.store(configured, atomic::Ordering::Relaxed);
+    }
+}
--- src/templates/wifi_http_client.rs
+++ src/templates/wifi_http_client.rs
@@ -1,182 +1,6 @@
-{% case template -%}
-{%- when "GPIO In/Out" -%}
-#![no_std]
-#![no_main]
-
-use defmt::info;
-use embassy_executor::Spawner;
-use embassy_rp as rp;
-use embassy_time as time;
-use embedded_hal_1 as hal;
-use embedded_hal_async as hal_async;
-use {defmt_rtt as _, panic_probe as _};
-
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    let (pin_sw, pin_led) = {
-        let p = rp::init(Default::default());
-        let pin_sw = rp::gpio::Input::new(p.PIN_15, rp::gpio::Pull::Up);
-        let pin_led = rp::gpio::Output::new(p.PIN_25, rp::gpio::Level::Low);
-        (pin_sw, pin_led)
-    };
-    let fut_task_pin_in_out = task_pin_in_out(pin_sw, pin_led);
-    fut_task_pin_in_out.await;
-}
-
-async fn task_pin_in_out<InputPin, OutputPin>(mut pin_sw: InputPin, mut pin_led: OutputPin)
-where
-    InputPin: hal::digital::InputPin + hal_async::digital::Wait,
-    OutputPin: hal::digital::OutputPin,
-{
-    info!("Starting task_pin_in_out");
-    loop {
-        let is_pushed = pin_sw.is_low().unwrap();
-        if is_pushed {
-            info!("Button pressed");
-            pin_led.set_high().ok();
-        } else {
-            info!("Button released");
-            pin_led.set_low().ok();
-        }
-        pin_sw.wait_for_any_edge().await.ok();
-        time::Timer::after_millis(30).await;    // debounce delay
-    }
-}
-{%- when "USB device CDC" -%}
-#![no_std]
-#![no_main]
-
-use core::sync::atomic;
-use defmt::info;
-use embassy_executor::Spawner;
-use embassy_rp as rp;
-use embassy_usb as usb;
-use static_cell::StaticCell;
-use {defmt_rtt as _, panic_probe as _};
-
-rp::bind_interrupts!(struct Irqs {
-    USBCTRL_IRQ => rp::usb::InterruptHandler<rp::peripherals::USB>;
-});
-
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    let (usb_driver,) = {
-        let p = rp::init(Default::default());
-        let usb_driver = rp::usb::Driver::new(p.USB, Irqs);
-        (usb_driver,)
-    };
-    let fut_task_usb = task_usb(usb_driver);
-    fut_task_usb.await;
-}
-
-async fn task_usb(usb_driver: impl usb::driver::Driver<'static>) {
-    let mut usb_builder = {
-        const VID: u16 = 0xc0de;
-        const PID: u16 = 0xcafe;
-        const CONFIG_DESCRIPTOR_SIZE: usize = 256;
-        const BOS_DESCRIPTOR_SIZE: usize = 256;
-        const MSOS_DESCRIPTOR_SIZE: usize = 256;
-        const CONTROL_BUF_SIZE: usize = 64;
-        let mut usb_config = usb::Config::new(VID, PID);
-        usb_config.manufacturer = Some("Embassy");
-        usb_config.product = Some("hoge");
-        usb_config.serial_number = Some("12345678");
-        usb_config.max_power = 100;
-        usb_config.max_packet_size_0 = CONTROL_BUF_SIZE as u8;
-        let config_descriptor_buf = {
-            static STATIC_CELL: StaticCell<[u8; CONFIG_DESCRIPTOR_SIZE]> = StaticCell::new();
-            STATIC_CELL.init([0u8; CONFIG_DESCRIPTOR_SIZE])
-        };
-        let bos_descriptor_buf = {
-            static STATIC_CELL: StaticCell<[u8; BOS_DESCRIPTOR_SIZE]> = StaticCell::new();
-            STATIC_CELL.init([0u8; BOS_DESCRIPTOR_SIZE])
-        };
-        let msos_descriptor_buf = {
-            static STATIC_CELL: StaticCell<[u8; MSOS_DESCRIPTOR_SIZE]> = StaticCell::new();
-            STATIC_CELL.init([0u8; MSOS_DESCRIPTOR_SIZE])
-        };
-        let control_buf = {
-            static STATIC_CELL: StaticCell<[u8; CONTROL_BUF_SIZE]> = StaticCell::new();
-            STATIC_CELL.init([0u8; CONTROL_BUF_SIZE])
-        };
-        let usb_handler = {
-            static STATIC_CELL: StaticCell<USBHandler> = StaticCell::new();
-            STATIC_CELL.init(USBHandler::new())
-        };
-        let mut usb_builder = usb::Builder::new(usb_driver, usb_config,
-            config_descriptor_buf, bos_descriptor_buf, msos_descriptor_buf, control_buf);
-        usb_builder.handler(usb_handler);
-        usb_builder
-    };
-    let cdc_driver = {
-        let state = {
-            static STATE: StaticCell<usb::class::cdc_acm::State> = StaticCell::new();
-            STATE.init(usb::class::cdc_acm::State::new())
-        };
-        let max_packet_size = 64;
-        usb::class::cdc_acm::CdcAcmClass::new(&mut usb_builder, state, max_packet_size)
-    };
-    let mut usb_device = usb_builder.build();
-    let fut_usb = usb_device.run();
-    let fut_echo = async {
-        let (mut cdc_sender, mut cdc_receiver) = cdc_driver.split();
-        let buf = {
-            static STATIC_CELL: StaticCell<[u8; 64]> = StaticCell::new();
-            STATIC_CELL.init([0u8; 64])
-        };
-        loop {
-            cdc_receiver.wait_connection().await;
-            info!("Connected");
-            let e = loop {
-                let buf_read = match cdc_receiver.read_packet(buf).await {
-                    Ok(n) => &buf[..n], Err(e) => break e,
-                };
-                if let Err(e) = cdc_sender.write_packet(buf_read).await { break e; }
-            };
-            if e != usb::driver::EndpointError::Disabled { break; }
-        };
-    };
-    embassy_futures::join::join(fut_usb, fut_echo).await;
-}
-
-struct USBHandler {
-    configured: atomic::AtomicBool,
-}
-
-impl USBHandler {
-    fn new() -> Self {
-        USBHandler { configured: atomic::AtomicBool::new(false), }
-    }
-}
-
-impl usb::Handler for USBHandler {
-    /// Called when the USB device has been enabled or disabled.
-    fn enabled(&mut self, enabled: bool) {
-        info!("usb::Handler.enabled({})", enabled);
-        self.configured.store(false, atomic::Ordering::Relaxed);
-    }
-    /// Called after a USB reset after the bus reset sequence is complete.
-    fn reset(&mut self) {
-        info!("usb::Handler.reset()");
-        self.configured.store(false, atomic::Ordering::Relaxed);
-    }
-    /// Called when the host has set the address of the device to `addr`.
-    fn addressed(&mut self, addr: u8) {
-        info!("usb::Handler.addressed(addr: {})", addr);
-        self.configured.store(false, atomic::Ordering::Relaxed);
-    }
-    /// Called when the host has enabled or disabled the configuration of the device.
-    fn configured(&mut self, configured: bool) {
-        info!("usb::Handler.configured(configured: {})", configured);
-        self.configured.store(configured, atomic::Ordering::Relaxed);
-    }
-}
-{%- when "Wi-Fi HTTP Client" -%}
-#![no_std]
-#![no_main]
-
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_futures as futures;
 use embassy_net as net;
 use embassy_rp as rp;
 use embassy_time as time;
@@ -190,10 +14,15 @@ rp::bind_interrupts!(struct Irqs {
     DMA_IRQ_0 => rp::dma::InterruptHandler<rp::peripherals::DMA_CH0>;
 });
 
+#[embassy_executor::task]
+async fn cyw43_task(cyw43_runner: cyw43::Runner<'static, cyw43::SpiBus<rp::gpio::Output<'static>, cyw43_pio::PioSpi<'static, rp::peripherals::PIO0, 0>>>) -> ! {
+    cyw43_runner.run().await;
+}
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    let (net_driver, mut cyw43_control, cyw43_runner, cyw43_clm) =  {
+    let (net_driver, mut cyw43_control) =  {
         const PRE_DOWNLOAD_FIRMWARE: bool = true;
         let pin_pwr = p.PIN_23;
         let pin_dio = p.PIN_24;
@@ -232,8 +61,16 @@ async fn main(_spawner: Spawner) {
             (fw, cyw43_clm)
         };
         let nvram = cyw43::aligned_bytes!("../cyw43-firmware/nvram_rp2040.bin");
-        let (net_driver, cyw43_control, cyw43_runner) = cyw43::new(state, pwr, spi, fw, nvram).await;
-        (net_driver, cyw43_control, cyw43_runner, cyw43_clm)
+        let (net_driver, mut cyw43_control, cyw43_runner) = cyw43::new(state, pwr, spi, fw, nvram).await;
+        spawner.spawn(cyw43_task(cyw43_runner).unwrap());
+        cyw43_control.init(cyw43_clm).await;
+        cyw43_control.set_power_management(cyw43::PowerManagementMode::PowerSave).await;
+        let ssid = private_info::WIFI_SSID;
+        let join_options = cyw43::JoinOptions::new(private_info::WIFI_PASSWORD.as_bytes());
+        while let Err(err) = cyw43_control.join(ssid, join_options.clone()).await {
+            info!("join failed: {:?}", err);
+        }
+        (net_driver, cyw43_control)
     };
     let (net_stack, mut net_runner) = {
         const USE_DHCP: bool = true;
@@ -255,17 +92,8 @@ async fn main(_spawner: Spawner) {
         let (net_stack, net_runner) = net::new(net_driver, config, resources, seed);
         (net_stack, net_runner)
     };
-    let fut_cys43_runner = cyw43_runner.run();
     let fut_net_runner = net_runner.run();
     let fut_main = async {
-        cyw43_control.init(cyw43_clm).await;
-        cyw43_control.set_power_management(cyw43::PowerManagementMode::PowerSave).await;
-        while let Err(err) = cyw43_control
-            .join(private_info::WIFI_NETWORK, cyw43::JoinOptions::new(private_info::WIFI_PASSWORD.as_bytes()))
-            .await
-        {
-            info!("join failed: {:?}", err);
-        }
         info!("waiting for link...");
         net_stack.wait_link_up().await;
         info!("waiting for DHCP...");
@@ -273,7 +101,15 @@ async fn main(_spawner: Spawner) {
         info!("Stack is up!");
         run_http_client(net_stack).await;
     };
-    embassy_futures::join::join3(fut_main, fut_cys43_runner, fut_net_runner).await;
+    let fut_blinky = async {
+        loop {
+            cyw43_control.gpio_set(0, true).await;
+            time::Timer::after_millis(300).await;
+            cyw43_control.gpio_set(0, false).await;
+            time::Timer::after_millis(300).await;
+        }
+    };
+    futures::join::join3(fut_main, fut_net_runner, fut_blinky).await;
 }
 
 async fn run_http_client(net_stack: net::Stack<'_>) -> ! {
@@ -365,4 +201,3 @@ async fn run_http_client(net_stack: net::Stack<'_>) -> ! {
         time::Timer::after_secs(5).await;
     }
 }
-{%- endcase -%}
